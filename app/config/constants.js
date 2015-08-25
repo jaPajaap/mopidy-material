29ec982c263e680a622e19b755dc90b4a025db4a
Jaap Gerritsen <jaapgerritsen@gmail.com>
2015-08-25 08:24:47 +0200

    more config

--- app/config/constants.js
+++ app/config/constants.js
@@ -5,7 +5,7 @@ var AppSettings = {
   starredPlaylistId: '45AW0U4mHoXBA7GEKeE5Jq',
   spotifyClientId: '453d62ba1a244e8780eb53e3fcc4ccac',
   appUrl: 'http://192.168.1.103:6680/material',
-  mopidyServer: 'localhost:6680'
+  mopidyServer: '192.168.1.103:6680'
 };
 
 module.exports = AppSettings;
