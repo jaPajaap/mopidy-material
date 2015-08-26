539f680a284b36741d23d1c9cff88f02a9ef345a
Jaap Gerritsen <jaapgerritsen@gmail.com>
2015-08-27 00:13:39 +0200

    config for local vs pi

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,10 +1,16 @@
 'use strict';
 
+var mopidyHost = '192.168.1.103:6680';
+var hostname = window.location.hostname;
+var host = window.location.host;
+
+var appUrl = hostname === 'localhost' ? host : 'http://192.168.1.103:6680/material';
+
 var AppSettings = {
   appTitle: 'Mopidy',
   starredPlaylistId: '45AW0U4mHoXBA7GEKeE5Jq',
   spotifyClientId: '453d62ba1a244e8780eb53e3fcc4ccac',
-  appUrl: 'http://192.168.1.103:6680/material',
+  appUrl: appUrl,
   mopidyServer: '192.168.1.103:6680'
 };
 
